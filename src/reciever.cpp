1b9f5643e700bef328f84b4b693d6d9375ccd5b2
Camleaf <bdaace09@gmail.com>
2025-03-27 19:06:18 -0400

    serial addition

--- src/reciever.cpp
+++ src/reciever.cpp
@@ -8,6 +8,7 @@ const byte address[6] = "00001";
 
 void setup() {
   Serial.begin(9600);
+  Serial.write("Listening");
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
--- src/transmitter.cpp
+++ src/transmitter.cpp
@@ -10,6 +10,8 @@ RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
 
 void setup() {
+  Serial.begin(9600);
+  Serial.write("Transmitting");
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
